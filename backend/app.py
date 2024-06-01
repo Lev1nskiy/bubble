f22677a33bf8784b4c29dbc150613df30b98832a
ledsley <lesha.sidorinko@yandex.ru>
2024-06-01 15:27:06 +0300

    update up

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 import mysql.connector
+from mysql.connector import connect, Error, MySQLConnection
 from collections import namedtuple
 import sqlite3
 import os
 import re
 from math import *
-from flask import Flask, render_template, url_for,  request, flash, session, redirect, abort, g
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
 from FDataBase import FDataBase
 from UserLogin import UserLogin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,27 +13,32 @@ from flask_login import LoginManager, login_user, login_required, current_user, 
 from flask_login import UserMixin, login_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='dsajlsahds78dasda54cf'
+app.config['SECRET_KEY'] = 'dsajlsahds78dasda54cf'
+
+
 
 login_manager = LoginManager(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     print("load_user")
     return UserLogin().fromDB(user_id, dbase)
 
-#========================= База Данных SQLite3 ==============================
+
+# ========================= База Данных SQLite3 ==============================
 
 def connect_db():
     # Подключение к базе данных
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="admin_sfedu",  # Замените на свой пароль
-        database="g-portal",
+        password="1111",  # Замените на свой пароль
+        database="project",
     )
     conn.row_factory = lambda cursor, row: namedtuple('Row', [x[0] for x in cursor.description])(*row)
     return conn
+
 
 def create_database():
     # Вспомогательная функция для создания таблиц базы данных
@@ -42,13 +48,17 @@ def create_database():
     db.commit()
     db.close()
 
+
 def get_db():
     # Соединение с базой данных, если оно ещё не установлено
-    if not hasattr(g,'link_db'):
+    if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
 
+
 dbase = None
+
+
 @app.before_request
 def before_request():
     # Установление соединения с базой данных перед выполнением запроса
@@ -56,39 +66,44 @@ def before_request():
     db = get_db()
     dbase = FDataBase(db)
 
+
 @app.teardown_appcontext
 def close_db(error):
     # Закрываем соединение с базой данных, если оно было установлено
-    if hasattr(g,'link_db'):
+    if hasattr(g, 'link_db'):
         g.link_db.close()
 
-#============================================================================
+
+# ============================================================================
 
 
-#========================Проверка данных регистрации=========================
+# ========================Проверка данных регистрации=========================
 def is_valid_name(name):
-    #Проверяет, является ли имя допустимым.
+    # Проверяет, является ли имя допустимым.
 
     pattern = r"^[A-ZА-ЯЁ]+[\sa-zA-Zа-яёА-ЯЁ]+$"
     return bool(re.match(pattern, name))
 
+
 # Условия проверки фамилии
 def is_valid_surname(surname):
-    #Проверяет, является ли фамилия допустимой.
+    # Проверяет, является ли фамилия допустимой.
 
     pattern = r"^[A-ZА-ЯЁ]+[\sa-zA-Zа-яёА-ЯЁ]+$"
     return bool(re.match(pattern, surname))
 
+
 # Условия проверки отчества
 def is_valid_patronymic(patronymic):
-    #Проверяет, является ли отчество допустимым.
+    # Проверяет, является ли отчество допустимым.
 
     pattern = r"^[A-ZА-ЯЁ]+[\sa-zA-Zа-яёА-ЯЁ]+$"
     return bool(re.match(pattern, patronymic))
 
+
 # Условия проверки логина
 def is_valid_login(login):
-    #Проверяет, является ли логин допустимым.
+    # Проверяет, является ли логин допустимым.
 
     pattern = r"^[a-zA-Z0-9]+[\sa-zA-Z0-9]+$"
     return bool(re.match(pattern, login))
@@ -96,14 +111,15 @@ def is_valid_login(login):
 
 # Условия проверки email
 def is_valid_email(email):
-    #Проверяет, является ли адрес электронной почты допустимым.
+    # Проверяет, является ли адрес электронной почты допустимым.
 
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     return bool(re.match(pattern, email))
 
+
 # Условия проверки пароля
 def is_valid_password(password):
-    #Проверяет, является ли пароль допустимым.
+    # Проверяет, является ли пароль допустимым.
 
     # Минимальная длина пароля
     min_length = 8
@@ -121,10 +137,12 @@ def is_valid_password(password):
     has_special_char = any(char in "!@#$%^&*()" for char in password)
 
     return len(password) >= min_length and has_number and has_lowercase and has_uppercase and has_special_char
-#============================================================================
 
 
-#======================= Структура меню навигации ===========================
+# ============================================================================
+
+
+# ======================= Структура меню навигации ===========================
 
 menu = [
     {
@@ -136,10 +154,10 @@ menu = [
         'url': 'qualification_check',
     },
 
-    #{
+    # {
     #    'name': 'Задонатить',
     #    'url': 'gratitude',
-    #},
+    # },
     {
         'name': 'Курсы',
         'url': 'contest',
@@ -150,32 +168,147 @@ menu = [
     },
 ]
 
-#============================================================================
+
+# ============================================================================
 
 
-#===============================Страницы сайта===============================
+# ===============================Страницы сайта===============================
 
 
 # ============================= Главная странца =============================
 @app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html', title = 'Главная страница сайта', menu = get_menu())
+    return render_template('index.html', title='Главная страница сайта', menu=get_menu())
+
 
 # ===================== Страница проверки квалификации ======================
 @app.route('/qualification-check')
 def qualification_check():
-    return render_template('qualification-check.html', title = 'Проверка квалификации', menu = get_menu())
+    return render_template('qualification-check.html', title='Проверка квалификации', menu=get_menu())
+
 
 # =========================== Страница задонатить ===========================
 '''@app.route('/gratitude')
 def gratitude():
     return render_template('gratitude.html', title = 'Задонатить', menu = get_menu())'''
 
+
 # ============================ Страница курсов ==============================
-@app.route('/contest')
+
+@app.route('/contest' , methods = ['GET', 'POST'])
 def contest():
-    return render_template('contest.html', title = 'Курсы', menu = get_menu())
+    if request.method == 'POST':
+        course_name = request.form['name']
+        course_theme = request.form['theme']
+        course_points = request.form['points']
+
+        dataBase = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="1111",
+            database="project"
+        )
+
+        cursorObject = dataBase.cursor()
+
+        sql= """
+        INSERT INTO courses
+        (name, theme, points)
+        VALUES ( %s, %s, %s )
+        """
+        val = [(course_name, course_theme, course_points)]
+
+        cursorObject.executemany(sql, val)
+        dataBase.commit()
+
+        dataBase.close()
+        return redirect('/contest')
+    else:
+        return render_template('contest.html', title='Курсы', menu=get_menu())
+
+#====================================доступные курсы============================================
+@app.route("/open_courses")
+def open_courses():
+    dataBase = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="1111",
+        database="project"
+    )
+
+    cursorObject = dataBase.cursor()
+
+    query = "SELECT NAME, theme FROM courses where id = 1"
+    cursorObject.execute(query)
+
+    results = cursorObject.fetchall()
+
+    for x in results:
+        print(x)
+
+    dataBase.close()
+    return render_template('open_courses.html', results = results)
+
+
+
+# ========================== Лидерборд ============================
+
+@app.route("/leaderboard")
+
+def leaderboard():
+    dataBase = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="1111",
+        database="project"
+    )
+
+    cursorObject = dataBase.cursor()
+
+    query = ("delete from leaderboard ")
+    cursorObject.execute(query)
+
+    dataBase.commit()
+
+    dataBase.close()
+
+    dataBase = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="1111",
+        database="project"
+    )
+
+    cursorObject = dataBase.cursor()
+
+    query = ("INSERT INTO leaderboard(user_id,points) SELECT  id,progress from users order by progress desc limit 10 ")
+    cursorObject.execute(query)
+
+    dataBase.commit()
+
+    dataBase.close()
+
+    dataBase = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="1111",
+        database="project"
+    )
+
+    cursorObject = dataBase.cursor()
+
+    query = "select name,surname,patronymic,points from users join leaderboard on users.id = leaderboard.user_id where progress >0;"
+    cursorObject.execute(query)
+
+    results = cursorObject.fetchall()
+
+    for x in results:
+        print(x)
+
+    dataBase.close()
+
+    return render_template('leaderboard.html', results = results)
 
 # ========================== Страница авторизации ============================
 @app.route("/login", methods=["POST", "GET"])
@@ -192,16 +325,20 @@ def login():
             else:
                 flash('Неверная пара логин/пароль', 'error')
         else:
-            print ("В регистрацию попал\n")
-            print (is_valid_name(request.form['name']))
-            print (is_valid_surname(request.form['surname']))
-            print (is_valid_patronymic(request.form['patronymic']))
-            print (is_valid_login(request.form['login']))
-            print (is_valid_email(request.form['email']))
+            print("В регистрацию попал\n")
+            print(is_valid_name(request.form['name']))
+            print(is_valid_surname(request.form['surname']))
+            print(is_valid_patronymic(request.form['patronymic']))
+            print(is_valid_login(request.form['login']))
+            print(is_valid_email(request.form['email']))
             # Регистрация
-            if is_valid_name(request.form['name']) and is_valid_surname(request.form['surname']) and is_valid_patronymic(request.form['patronymic']) and is_valid_login(request.form['login']) and is_valid_email(request.form['email']) and is_valid_password(request.form['psw1']) and request.form['psw1'] == request.form['psw2']:
+            if is_valid_name(request.form['name']) and is_valid_surname(
+                    request.form['surname']) and is_valid_patronymic(request.form['patronymic']) and is_valid_login(
+                    request.form['login']) and is_valid_email(request.form['email']) and is_valid_password(
+                    request.form['psw1']) and request.form['psw1'] == request.form['psw2']:
                 hash = generate_password_hash(request.form['psw1'])
-                res = dbase.addUser(request.form['name'], request.form['surname'], request.form['patronymic'], request.form['login'], request.form['email'], hash, 'user')
+                res = dbase.addUser(request.form['name'], request.form['surname'], request.form['patronymic'],
+                                    request.form['login'], request.form['email'], hash, 'user')
                 if res:
                     flash('Пользователь успешно зарегистрирован', "success")
                     return redirect(url_for('login'))
@@ -212,8 +349,8 @@ def login():
 
     return render_template("login.html", menu=menu, title="Авторизация")
 
- 
     return render_template("login.html", menu=menu, title="Авторизация")
+
 
 # ======================== Страница после выхода ============================
 @app.route('/logout')
@@ -223,13 +360,18 @@ def logout():
     flash("Вы вышли из аккаунта", "success")
     return redirect(url_for('login'))
 
+
 # ========================== Страница профиля ===============================
-@app.route("/profile/add_user", methods = ['POST', 'GET'])
+@app.route("/profile/add_user", methods=['POST', 'GET'])
 def add_user():
     if request.method == 'POST':
-        if is_valid_name(request.form['name']) and is_valid_surname(request.form['surname']) and is_valid_patronymic(request.form['patronymic']) and is_valid_login(request.form['login']) and is_valid_email(request.form['email']) and is_valid_password(request.form['psw1']) and request.form['psw1'] == request.form['psw2']:
+        if is_valid_name(request.form['name']) and is_valid_surname(request.form['surname']) and is_valid_patronymic(
+                request.form['patronymic']) and is_valid_login(request.form['login']) and is_valid_email(
+                request.form['email']) and is_valid_password(request.form['psw1']) and request.form['psw1'] == \
+                request.form['psw2']:
             hash = generate_password_hash(request.form['psw1'])
-            res = dbase.addUser(request.form['name'], request.form['surname'], request.form['patronymic'], request.form['login'], request.form['email'], hash, request.form['role'])
+            res = dbase.addUser(request.form['name'], request.form['surname'], request.form['patronymic'],
+                                request.form['login'], request.form['email'], hash, request.form['role'])
             if res:
                 flash('Пользователь успешно зарегистрирован', "success")
                 return redirect(url_for('profile'))
@@ -237,29 +379,34 @@ def add_user():
                 flash('Пользователь с таким логином или email уже существует.', "error")
         else:
             flash('Неправильно введены данные!', "error")
-    return render_template('add_user.html', title = 'Добавление пользователя', menu = get_menu())
+    return render_template('add_user.html', title='Добавление пользователя', menu=get_menu())
+
 
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', title = 'Профиль', menu = get_menu())
+    return render_template('profile.html', title='Профиль', menu=get_menu())
 
 
-            
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page404.html', title = 'Страница не найдена', menu = get_menu()), 404
+    return render_template('page404.html', title='Страница не найдена', menu=get_menu()), 404
+
+
 @app.errorhandler(401)
 def unauthorized(error):
-    return render_template('page401.html', title = 'Вы не авторизованы', menu = get_menu()), 401
-#============================================================================
+    return render_template('page401.html', title='Вы не авторизованы', menu=get_menu()), 401
+
+
+# ============================================================================
 
 def get_user_info():
     if current_user.is_authenticated:
         return current_user.get_name(), current_user.get_surname()
     else:
         return None
-    
+
+
 def get_menu():
     new_menu = menu[:-1].copy()
     user_info = get_user_info()
@@ -275,9 +422,10 @@ def get_menu():
         })
     return new_menu
 
-#=========================== Запуск веб-приложения ==========================
+
+# =========================== Запуск веб-приложения ==========================
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-#============================================================================
+
+# ============================================================================
